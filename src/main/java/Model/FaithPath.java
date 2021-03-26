5f58896fe352078138fb280db36780476e320d48
Emanuele <emanuele.aulenti@mail.polimi.it>
2021-03-26 17:25:35 +0100

    added FaithPath

---
+++ src/main/java/Model/FaithPath.java
@@ -0,0 +1,11 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class FaithPath {
+    private ArrayList<Integer> positions;
+    private ArrayList<Character> faithTrack;
+    private ArrayList<Integer> victoryPoints;
+    private int papalFavourCounter;
+
+}
