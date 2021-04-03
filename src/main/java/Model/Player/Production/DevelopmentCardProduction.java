9c05d621c36d829596aef4e5e30890aa1251d6e2
Emanuele <emanuele.aulenti@mail.polimi.it>
2021-04-03 12:41:10 +0200

    implemented DevelopmentCardProduction

--- src/main/java/Model/Player/Production/DevelopmentCardProduction.java
+++ src/main/java/Model/Player/Production/DevelopmentCardProduction.java
@@ -2,10 +2,9 @@ package Model.Player.Production;
 
 import Model.Cards.*;
 import Model.Deck;
+import Model.Resources.ResourceContainer;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 
 public class DevelopmentCardProduction implements ProductionSlot {
     private Deck dev;
@@ -14,14 +13,39 @@ public class DevelopmentCardProduction implements ProductionSlot {
         dev = new Deck();
     }
 
+    public boolean insertOnTop (DevelopmentCard developmentCard) {
+        if(dev.getDeck().peek() != null)
+            dev.getDeck().element().changeStatus(Status.ACTIVE);
+
+        developmentCard.changeStatus(Status.ON_TOP);
+        dev.getDeck().add(developmentCard);
+
+        return true;
+    }
+
     @Override
-    public int countCardsWith(int level, Colour c){
+    public ArrayList<ResourceContainer> getProductionInput() {
+        if(dev.getDeck().peek() != null)
+            return dev.getDeck().element().getInput();
+        else
+            return null;
+    }
+
+    @Override
+    public ArrayList<ResourceContainer> getProducionOutput() {
+        if(dev.getDeck().peek() != null)
+            return dev.getDeck().element().getOutput();
+        else
+            return null;
+    }
+
+    @Override
+    public int countCardsWith(int level, Colour c) {
         int i = 0;
         for (DevelopmentCard d: dev.getDeck()) {
-            if (d.isSameLevelandColour(level, c));
+            if (d.isSameLevelandColour(level, c))
                 i++;
         }
-
         return i;
     }
 }
