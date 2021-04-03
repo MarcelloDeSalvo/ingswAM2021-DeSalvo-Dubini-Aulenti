3ec4fd9b05a7a650138279e075f67a516440f382
Francesco <franceso.dubini@mail.polimi.it>
2021-04-03 18:29:04 +0200

    Started implementing Market and it's constructor

--- src/main/java/Model/Market.java
+++ src/main/java/Model/Market.java
@@ -1,4 +1,37 @@
 package Model;
 
+import Model.Resources.ResourceContainer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Market {
+    ResourceContainer[][] market= new ResourceContainer[4][3];
+    ResourceContainer vacant;
+
+    public Market(ArrayList<ResourceContainer> marblesMarket){
+        Collections.shuffle(marblesMarket);
+        for(int i=0; i<4;i++){
+            for(int j=0;j<3;j++){
+                this.market[i][j]=marblesMarket.get(i*3+j);
+                //System.out.print(market[i][j].getResourceType()+"      |      ");
+            }
+            //System.out.println();
+        }
+
+        for(int i=0; i<3;i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(market[j][i].getResourceType() + "   |   ");
+            }
+            System.out.println();
+        }
+            this.vacant=marblesMarket.get(12);
+    }
+
+
+
+
+
+
+
 }
---
+++ src/test/java/Model/MarketTest.java
@@ -0,0 +1,48 @@
+package Model;
+
+import Model.Resources.ResourceContainer;
+import Model.Resources.ResourceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketTest {
+    ArrayList<ResourceContainer> marblesMarket= new  ArrayList<>();
+    @BeforeEach
+    void constructRandomMarbles(){
+        for(int i=0; i<13; i++) {
+            Random random=new Random();
+            int randomNum= random.nextInt(6);
+            switch (randomNum){
+                case 0:
+                    marblesMarket.add(new ResourceContainer(ResourceType.GOLD,1));
+                case 1:
+                    marblesMarket.add(new ResourceContainer(ResourceType.MINION,1));
+                case 2:
+                    marblesMarket.add(new ResourceContainer(ResourceType.STONE  ,1));
+                case 3:
+                    marblesMarket.add(new ResourceContainer(ResourceType.SHIELD,1));
+                case 4:
+                    marblesMarket.add(new ResourceContainer(ResourceType.FAITHPOINT,1));
+                case 5:
+                    marblesMarket.add(new ResourceContainer(ResourceType.BLANK,1));
+              /*  default:
+                    System.out.println("Errore!");*/
+            }
+        }
+    }
+
+
+    @Test
+    void marketConstructor(){
+
+        Market market=new Market(marblesMarket);
+
+        System.out.println("Il vacant è: "+market.vacant.getResourceType());
+    }
+
+}
