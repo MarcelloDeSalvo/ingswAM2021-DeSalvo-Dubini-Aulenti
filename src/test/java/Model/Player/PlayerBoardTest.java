18aaa028d0447ceb1d32eabecccc9c556b96b195
MARCELLO <marcello.desalvo@mail.polimi.it>
2021-04-07 11:38:26 +0200

    Added PlayerBoardTest Class

---
+++ src/test/java/Model/Player/PlayerBoardTest.java
@@ -0,0 +1,36 @@
+package Model.Player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerBoardTest {
+
+    @Test
+    void checkResources() {
+    }
+
+    @Test
+    void canProduce() {
+    }
+
+    @Test
+    void produce() {
+    }
+
+    @Test
+    void canBuy() {
+    }
+
+    @Test
+    void arraylistToMap() {
+    }
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void insertBoughtCard() {
+    }
+}
