32a7f09a5648144597c22e2765268f1ee72d7f01
Francesco <franceso.dubini@mail.polimi.it>
2021-04-10 15:50:24 +0200

    Finished tests for Production Ability

--- src/test/java/Model/Cards/ProductionAbilityTest.java
+++ src/test/java/Model/Cards/ProductionAbilityTest.java
@@ -28,4 +28,35 @@ class ProductionAbilityTest {
 
     }
 
+    @Test
+    void productionAbility2() {
+        LeaderCard bakerLeader= new LeaderCard(8);
+        ArrayList<ResourceContainer> inputCard1= new ArrayList<>();
+        ArrayList<ResourceContainer> outputCard1= new ArrayList<>();
+        inputCard1.add(new ResourceContainer(ResourceType.GOLD,1));
+        outputCard1.add(new ResourceContainer(ResourceType.MINION,1));
+        ProductionAbility bake=new ProductionAbility(inputCard1,outputCard1,1,1);
+        bakerLeader.addAbility(bake);
+        LeaderCard cookLeader= new LeaderCard(5);
+        ArrayList<ResourceContainer> inputCard2= new ArrayList<>();
+        ArrayList<ResourceContainer> outputCard2= new ArrayList<>();
+        inputCard2.add(new ResourceContainer(ResourceType.STONE,1));
+        outputCard2.add(new ResourceContainer(ResourceType.GOLD,1));
+        ProductionAbility cook=new ProductionAbility(inputCard2,outputCard2,0,0);
+        bakerLeader.addAbility(cook);
+        Player gianEnrico= new Player("ing.Conti");
+        bakerLeader.executeAbility(gianEnrico.getPlayerBoard());
+        cookLeader.executeAbility(gianEnrico.getPlayerBoard());
+
+        assertEquals(gianEnrico.getPlayerBoard().getProductionSite().getProductionSlotByID(4).getProductionInput().get(0).getResourceType(), ResourceType.GOLD);
+        assertEquals(gianEnrico.getPlayerBoard().getProductionSite().getProductionSlotByID(4).getProductionOutput().get(0).getResourceType(), ResourceType.MINION);
+        assertEquals(gianEnrico.getPlayerBoard().getProductionSite().getProductionSlotByID(5).getProductionInput().get(0).getResourceType(), ResourceType.STONE);
+        assertEquals(gianEnrico.getPlayerBoard().getProductionSite().getProductionSlotByID(5).getProductionOutput().get(0).getResourceType(), ResourceType.GOLD);
+
+        assertThrows(NullPointerException.class, ()-> gianEnrico.getPlayerBoard().getProductionSite().getProductionSlotByID(6).getProductionInput().get(0).getResourceType());
+
+    }
+
 }
+
+
