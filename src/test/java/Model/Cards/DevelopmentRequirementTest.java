447b759d7678f63ee3dedfe3bd236e7ff592ec3e
MARCELLO <marcello.desalvo@mail.polimi.it>
2021-04-11 14:22:30 +0200

    Fixed some values inside LeaderCards.json

--- src/test/java/Model/Cards/DevelopmentRequirementTest.java
+++ src/test/java/Model/Cards/DevelopmentRequirementTest.java
@@ -27,13 +27,40 @@ class DevelopmentRequirementTest {
         leaderCards = LeaderCardParser.deserializeLeaderList();
         developmentCards = DevelopmentCardParser.deserializeDevelopmentList();
 
+        //Green level 3
         assertTrue(p_playerBoard.insertBoughtCard(p_playerBoard.getProductionSlotByID(1),developmentCards.get(0)));
-        assertTrue(p_playerBoard.insertBoughtCard(p_playerBoard.getProductionSlotByID(2), developmentCards.get(35)));
+        assertEquals(developmentCards.get(1).getColour(), Colour.GREEN);
+        assertEquals(developmentCards.get(1).getLevel(), 3);
+
+        //Blue level 2
+        assertTrue(p_playerBoard.insertBoughtCard(p_playerBoard.getProductionSlotByID(2), developmentCards.get(16)));
+        assertEquals(developmentCards.get(16).getColour(), Colour.BLUE);
+        assertEquals(developmentCards.get(16).getLevel(), 2);
+
+        //Yellow level 1
+        assertTrue(p_playerBoard.insertBoughtCard(p_playerBoard.getProductionSlotByID(3), developmentCards.get(35)));
+        assertEquals(developmentCards.get(3).getColour(), Colour.YELLOW);
+        assertEquals(developmentCards.get(3).getLevel(), 1);
 
     }
 
     @Test
-    void checkRequirements(){
+    void checkRequirements_AnyLevel_True(){
+        //He needs one generic Green and one generic Yellow
         assertTrue(leaderCards.get(0).checkRequirements(p_playerBoard));
+    }
+
+    @Test
+    void checkRequirements_AnyLevel_False(){
+        //He needs one generic Yellow and one generic Blue
+        assertFalse(leaderCards.get(1).checkRequirements(p_playerBoard));
+    }
+
+    @Test
+    void checkRequirements_SpecificLevel_False(){
+        //He needs one level 2 blue
+        assertTrue(leaderCards.get(12).checkRequirements(p_playerBoard));
+        //He needs one level 2 purple
+        assertFalse(leaderCards.get(11).checkRequirements(p_playerBoard));
     }
 }
