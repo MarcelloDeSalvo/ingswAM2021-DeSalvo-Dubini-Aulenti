361aee16ccdc8ff51bb9030d5af8c03972875631
Francesco <franceso.dubini@mail.polimi.it>
2021-04-15 18:09:13 +0200

    Worked on FaithPath; implemented activatePapalFavour and isFirstOnPopeSpace

--- src/main/java/Model/FaithPath.java
+++ src/main/java/Model/FaithPath.java
@@ -3,13 +3,14 @@ package Model;
 import java.util.ArrayList;
 
 public class FaithPath implements ObserverFaithPath, ObservableEndGame {
-    private int numberOfPlayers;
     private ArrayList<Integer> positions;
     private int length;
+    private int lastPActivated;
     private ArrayList<Character> vaticanReports;
     private ArrayList<Integer> victoryPoints;
     private ArrayList<ObserverEndGame> observersEndGame;
     private ArrayList<Integer> papalFavours;
+    private ArrayList<playerFavour> playersFavourList;
 
 
     /**
@@ -20,9 +21,12 @@ public class FaithPath implements ObserverFaithPath, ObservableEndGame {
         this.vaticanReports = vaticanReports;
         this.victoryPoints = victoryPoints;
         this.papalFavours = papalFavours;
-
+        this.lastPActivated=0;
         this.positions = new ArrayList<>();
         this.observersEndGame = new ArrayList<>();
+        this.playersFavourList=new ArrayList<>();
+        for(int i=0;i<Game.numOfPlayers;i++)
+            playersFavourList.add(new playerFavour());
         setUpPositions();
     }
 
@@ -36,29 +40,40 @@ public class FaithPath implements ObserverFaithPath, ObservableEndGame {
     }
 
     private void setUpPositions() {
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i <Game.numOfPlayers; i++)
             positions.add(0);
     }
 
 
     //FAITHPATH METHODS-------------------------------------------------------------------------------------------------
 
-    public boolean isOnPopeSpace(){
-        if(vaticanReports.get(positions.get(Game.currentPlayer)) == 'P')
+    /**
+     * Checks if current player is the first to land on a Pope Space
+     * @return
+     */
+    public boolean isFirstOnPopeSpace(){
+        if(vaticanReports.get(positions.get(Game.currentPlayer)) == 'P' && positions.get(Game.currentPlayer)>lastPActivated )
             return true;
         return false;
 
     }
 
+    /**
+     * Adds the top of the papalFavour stack to every eligible player. The papalFavour is now considered used, and <br>
+     * is thus  removed from the stack. To check if a player is on a Pope Space that has not yet been used, we update <br>
+     * the latest used P char by updating lastPActivated.
+     * @return true if there are no errors.
+     */
     public boolean activatePapalFavour(){
-        for(int i=0;i<numberOfPlayers-1;i++){
-            //Per assegnare i PapalFavour ai giocatori fai ArrayList di ArrayList e fai .add di quello flippato se va flippato
-
+        for(int i=0;i<Game.numOfPlayers;i++){
+            if((vaticanReports.get(positions.get(i)) == 'P' || vaticanReports.get(positions.get(i)) == 'X') && positions.get(i)>lastPActivated)
+                playersFavourList.get(i).addFavour(papalFavours.get(0));
         }
+        lastPActivated=positions.get(Game.currentPlayer);
+        papalFavours.remove(0);
 
         return true;
     }
-
 
 
     //------------------------------------------------------------------------------------------------------------------
@@ -69,7 +84,7 @@ public class FaithPath implements ObserverFaithPath, ObservableEndGame {
     public void update(int faithPoints) {
         for(int i=0;i<faithPoints;i++){
             positions.set(Game.currentPlayer, positions.get(Game.currentPlayer) + 1);
-            if(isOnPopeSpace()){
+            if(isFirstOnPopeSpace()){
                 activatePapalFavour();
             }
             victoryConditions();
@@ -127,15 +142,27 @@ public class FaithPath implements ObserverFaithPath, ObservableEndGame {
     public ArrayList<Integer> getPapalFavours() {
         return papalFavours;
     }
-
-    public void setNumberOfPlayers(int numberOfPlayers) {
-        this.numberOfPlayers = numberOfPlayers;
-    }
-
-    public int getNumberOfPlayers() {
-        return numberOfPlayers;
-    }
     //------------------------------------------------------------------------------------------------------------------
 }
 
 
+
+    //PlayerFavour (Support class)--------------------------------------------------------------------------------------
+
+class playerFavour{
+    private ArrayList<Integer> Favours;
+
+    public playerFavour() {
+        Favours = new ArrayList<>();
+    }
+
+    public boolean addFavour(int favourAmount){
+        Favours.add(favourAmount);
+        return true;
+    }
+
+    public ArrayList<Integer> getFavours() {
+        return Favours;
+    }
+
+}
