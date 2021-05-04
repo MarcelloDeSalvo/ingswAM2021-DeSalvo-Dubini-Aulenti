bd47fdba2c47f50fac488d229ca0e156393b5199
Francesco <franceso.dubini@mail.polimi.it>
2021-05-04 21:33:27 +0200

    Buy some sout"Hello!", now comes in cyan!

---
+++ src/main/java/it/polimi/ingsw/view/cli/Color.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.view.cli;
+
+public enum Color {
+    ANSI_RED("\u001B[31m"),
+    ANSI_GREEN("\u001B[32m"),
+    ANSI_YELLOW("\u001B[33m"),
+    ANSI_BLUE("\u001B[34m"),
+    ANSI_MAGENTA("\u001B[35m"),
+    ANSI_CYAN("\u001B[36m");
+
+    static final String RESET = "\u001B[0m";
+
+    private String escape;
+
+    Color(String escape) {
+        this.escape = escape;
+    }
+    public String escape(){
+        return escape;
+    }
+
+
+}
