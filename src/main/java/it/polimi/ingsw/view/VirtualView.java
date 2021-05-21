ebb280b78c4ee78eca253bd0a92206ab5fb59c01
Emanuele <emanuele.aulenti@mail.polimi.it>
2021-05-21 14:20:26 +0200

    Minor fixes

--- src/main/java/it/polimi/ingsw/view/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/VirtualView.java
@@ -243,7 +243,7 @@ public class VirtualView implements View {
 
     @Override
     public void notifyCardRemoved(int amount, Colour color, int level) {
-        printReply("LORENZO has removed "+ amount+ " "+color+ " development cards with level = " + level);
+        printReply("LORENZO has removed " + amount + " "+color+ " development cards with level = " + level);
     }
 
     @Override
