25e20bf254ff47d82f1d62693733d5d77e3b34de
MARCELLO <marcello.desalvo@mail.polimi.it>
2021-05-28 11:19:27 +0200

    - CLI: changed some too dark ANSI colors

--- src/main/java/it/polimi/ingsw/view/cli/Color.java
+++ src/main/java/it/polimi/ingsw/view/cli/Color.java
@@ -1,14 +1,14 @@
 package it.polimi.ingsw.view.cli;
 
 public enum Color {
-    ANSI_RED ("\u001B[31m"),
-    ANSI_YELLOW ("\u001B[33m"),
-    ANSI_BLUE ("\u001B[34m"),
-    ANSI_PURPLE ("\u001B[35m"),
+    ANSI_RED ("\u001B[38;5;196m"),
+    ANSI_YELLOW ("\u001B[38;5;220m"),
+    ANSI_BLUE ("\u001B[38;5;27m"),
+    ANSI_PURPLE ("\u001B[38;5;162"),
     ANSI_GRAY ("\u001B[37m"),
     ANSI_WHITE ("\u001B[0;1m"),
 
-    ANSI_GREEN ("\u001B[32m"),
+    ANSI_GREEN ("\u001B[38;5;46m"),
     ANSI_CYAN ("\u001B[36m"),
     ANSI_BLACK ("\u001B[30m"),
     ANSI_WHITE_BOLD_FRAMED("\u001B[51;1m"),
