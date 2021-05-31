d3d6ddc87d01db91885648ccfca0635d306b0808
MARCELLO <marcello.desalvo@mail.polimi.it>
2021-06-01 00:53:55 +0200

    - GUI: added a new BackgroundClass

--- src/main/java/it/polimi/ingsw/view/gui/panels/BackGroundImagePanelResize.java
+++ src/main/java/it/polimi/ingsw/view/gui/panels/BackGroundImagePanelResize.java
@@ -6,18 +6,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class LabelImage extends JPanel {
+public class BackGroundImagePanelResize extends JPanel {
 
-    private final BufferedImage imageLabel;
+    private BufferedImage imageLabel;
 
-    public LabelImage(String path){
+    public BackGroundImagePanelResize(String path){
         super();
+
         imageLabel =  ImageUtil.loadImage(path);
+
     }
+
 
     @Override
     public void paint(Graphics g){
         g.drawImage(imageLabel, 0, 0, this.getWidth(), this.getHeight(), null);
     }
-
 }
