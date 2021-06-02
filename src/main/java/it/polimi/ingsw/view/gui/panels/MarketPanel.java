fcbf236c63a299024f7b0636b483aa350d9efc35
Francesco <franceso.dubini@mail.polimi.it>
2021-06-02 16:35:37 +0200

    Finished the look of MarketPanel (Actual scans of the marbles are still missing)

--- src/main/java/it/polimi/ingsw/view/gui/panels/MarketPanel.java
+++ src/main/java/it/polimi/ingsw/view/gui/panels/MarketPanel.java
@@ -18,11 +18,11 @@ public class MarketPanel extends BackgroundImagePanel{
     private Gui gui;
 
     public MarketPanel(Gui gui, LiteMarket liteMarket) throws ImageNotFound{
-        super("/images/backgrounds/marketBackground.png", 600, 5, false);
+        super("/images/backgrounds/marketBackground.png", 650, 5, false);
         this.setLayout(new BorderLayout());
-        this.setBounds(0,0,900,950);
-        this.setWidth(900);
-        this.setHeight(950);
+        this.setBounds(0,0,850,900);
+        this.setWidth(696);
+        this.setHeight(900);
 
         this.gui=gui;
         this.liteMarket=liteMarket;
@@ -35,21 +35,70 @@ public class MarketPanel extends BackgroundImagePanel{
         mainPanel.setLayout(new BorderLayout());
         mainPanel.setOpaque(false);
 
-        JPanel marketAndVacant=new JPanel();
-        marketAndVacant.setLayout(new BoxLayout(marketAndVacant,BoxLayout.X_AXIS));
+        JPanel marketBox=new JPanel();
+        marketBox.setLayout(new BoxLayout(marketBox,BoxLayout.X_AXIS));
 
         showMarket();
 
-        marketAndVacant.setOpaque(false);
-        marketAndVacant.add(vacant);
-        marketAndVacant.add(Box.createHorizontalGlue());
-        marketAndVacant.add(marbles);
+        marketBox.setOpaque(false);
+        //marketAndVacant.add(vacant);
+        //marketAndVacant.add(Box.createHorizontalGlue());
+        marketBox.add(marbles);
 
 
-        marketAndVacant.setBorder(BorderFactory.createEmptyBorder(250,676,450,750) );
-        marketAndVacant.setBounds(250,676,400,700);
-        mainPanel.add(marketAndVacant,BorderLayout.CENTER);
+        JPanel rowButtons=new JPanel();
+        rowButtons.setLayout(new BoxLayout(rowButtons,BoxLayout.Y_AXIS));
+        rowButtons.setOpaque(false);
+        //rowButtons.add(Box.createVerticalGlue());
 
+        JPanel columnButtons= new JPanel();
+        columnButtons.setLayout(new BoxLayout(columnButtons,BoxLayout.X_AXIS));
+        columnButtons.setOpaque(false);
+        //columnButtons.add(Box.createHorizontalGlue());
+
+
+        rowButtons.add(Box.createRigidArea(new Dimension(0,15)));
+        for(int i=0;i<3;i++){
+            ButtonImage buttonImage=new ButtonImage(" + ",false);
+            final int j=i;
+            buttonImage.addActionListener(e->{
+                System.out.println("Riga numero "+(j+1));
+            });
+            rowButtons.add(buttonImage);
+            rowButtons.add(Box.createVerticalGlue());
+        }
+        rowButtons.add(Box.createRigidArea(new Dimension(0,117)));
+
+
+        columnButtons.add(Box.createRigidArea(new Dimension(770,0)));
+        for(int i=0;i<4;i++){
+            ButtonImage buttonImage=new ButtonImage(" + ",false);
+            final int j=i;
+            buttonImage.addActionListener(e->{
+                System.out.println("Colonna numero "+(j+1));
+            });
+            columnButtons.add(buttonImage);
+            columnButtons.add(Box.createHorizontalGlue());
+        }
+        columnButtons.add(Box.createRigidArea(new Dimension(235,0)));
+
+
+        marketBox.setBorder(BorderFactory.createEmptyBorder(0,81,127,28) );
+        //marketAndVacant.setBounds(290,676,360,700);
+
+        columnButtons.setBorder(BorderFactory.createEmptyBorder(30,0,0,0));
+        rowButtons.setBorder(BorderFactory.createEmptyBorder(0,130,0,40));
+        vacant.setBorder(BorderFactory.createEmptyBorder(0,0,0,400));
+
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(200,100,270,497) );
+
+
+        mainPanel.add(marketBox,BorderLayout.CENTER);
+
+
+        mainPanel.add(rowButtons,BorderLayout.EAST);
+        mainPanel.add(columnButtons,BorderLayout.SOUTH);
+        mainPanel.add(vacant,BorderLayout.WEST);
         this.add(mainPanel,BorderLayout.CENTER);
 
     }
@@ -63,13 +112,28 @@ public class MarketPanel extends BackgroundImagePanel{
         marbles.setOpaque(false);
 
         for (ResourceContainer marb :marbleArray) {
-            ButtonImage buttonImage=new ButtonImage("/images/marblesScuffed/marble"+marb.getResourceType().deColored().toLowerCase()+".png");
+            ButtonImage buttonImage=new ButtonImage("/images/marblesScuffed/marble"+marb.getResourceType().deColored().toLowerCase()+".png",new Dimension(60,60));
+            buttonImage.setBorderPainted(false);
             buttonImage.setOpaque(false);
             buttonImage.setContentAreaFilled(false);
             marbles.add(buttonImage);
         }
 
-        vacant.add(new ButtonImage("/images/marblesScuffed/marble"+liteMarket.getVacant().getResourceType().deColored().toLowerCase()+".png"));
+        vacant.setLayout(new BoxLayout(vacant, BoxLayout.Y_AXIS));
+        JLabel vacantText1=new JLabel("This is the ");
+        vacantText1.setVerticalAlignment(SwingConstants.CENTER);
+        vacantText1.setFont(new Font("Helvetica", Font.PLAIN, 40));
+
+        JLabel vacantText2=new JLabel("current vacant: ");
+        vacantText2.setVerticalAlignment(SwingConstants.CENTER);
+        vacantText2.setFont(new Font("Helvetica", Font.PLAIN, 40));
+
+        vacant.add(vacantText1);
+        vacant.add(vacantText2);
+
+        ButtonImage vacantMarble=new ButtonImage("/images/marblesScuffed/marble"+liteMarket.getVacant().getResourceType().deColored().toLowerCase()+".png");
+        vacantMarble.setBorderPainted(false);
+        vacant.add(vacantMarble);
         vacant.setOpaque(false);
     }
 
